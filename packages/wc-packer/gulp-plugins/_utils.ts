30f89098bac36c8a65e7408c03331adc9b33eafc
imhele <work@imhele.com>
2021-03-27 16:05:31 +0800

    chore: update packages info

--- packages/wc-packer/gulp-plugins/_utils.ts
+++
@@ -1,9 +0,0 @@
-import type * as File from 'vinyl';
-
-export function readVinylFile(file: File, encoding?: BufferEncoding): string | null {
-  let contents: string | Buffer | NodeJS.ReadableStream | null = file.contents;
-  if (contents === null) return null;
-  if (!(contents instanceof Buffer)) contents = contents.read();
-  if (contents instanceof Buffer) contents = contents.toString(encoding);
-  return contents;
-}
