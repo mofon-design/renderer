243b2cef61c8285c5e9173e0544b4b83539059e6
imhele <work@imhele.com>
2021-04-05 00:36:45 +0800

    feat(wc-bundler): resolve rollup config

---
+++ packages/wc-bundler/src/config/rollup/interface.ts
@@ -0,0 +1,182 @@
+import type { RollupBabelInputPluginOptions as RollupBabelConfig } from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
+import cjs from '@rollup/plugin-commonjs';
+import type { RollupJsonOptions as RollupJsonConfig } from '@rollup/plugin-json';
+import json from '@rollup/plugin-json';
+import type { RollupNodeResolveOptions as RollupNodeResolveConfig } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import url from '@rollup/plugin-url';
+import type { RollupOptions, Plugin as RollupPlugin } from 'rollup';
+import type { Options as RollupTerserConfig } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
+import { env } from '../../utils';
+
+export type RollupCommonJSConfig = typeof cjs extends (options?: infer Options) => unknown
+  ? Options
+  : never;
+
+export function DefaultRollupCommonJSConfig(): RollupCommonJSConfig {
+  return {};
+}
+
+export type { RollupBabelConfig, RollupJsonConfig, RollupNodeResolveConfig, RollupTerserConfig };
+
+export function DefaultRollupBabelConfig(): RollupBabelConfig {
+  return {};
+}
+
+export function DefaultRollupJsonConfig(): RollupJsonConfig {
+  return {};
+}
+
+export function DefaultRollupNodeResolveConfig(): RollupNodeResolveConfig {
+  return {};
+}
+
+export function DefaultRollupTerserConfig(): RollupTerserConfig {
+  return {};
+}
+
+export interface RollupUrlConfig {
+  /**
+   * A [minimatch pattern](https://github.com/isaacs/minimatch), or array of patterns,
+   * which specifies the files in the build the plugin should *ignore*.
+   * By default no files are ignored.
+   */
+  exclude?: string | string[];
+  /**
+   * A [minimatch pattern](https://github.com/isaacs/minimatch), or array of patterns,
+   * which specifies the files in the build the plugin should operate on.
+   * By default .svg, .png, .jpg, .jpeg, .gif and .webp files are targeted.
+   *
+   * @default
+   * ['**\/*.svg', '**\/*.png', '**\/*.jp(e)?g', '**\/*.gif', '**\/*.webp']
+   */
+  include?: string | string[];
+  /**
+   * The file size limit for inline files. If a file exceeds this limit, it will be copied to
+   * the destination folder and the hashed filename will be provided instead.
+   * If `limit` is set to `0` all files will be copied.
+   *
+   * @default 14336
+   */
+  limit?: number;
+  /**
+   * A string which will be added in front of filenames when they are not inlined but are copied.
+   *
+   * @default ''
+   */
+  publicPath?: string;
+  /**
+   * If `false`, will prevent files being emitted by this plugin.
+   * This is useful for when you are using Rollup to emit both a client-side and server-side bundle.
+   *
+   * @default true
+   */
+  emitFiles?: boolean;
+  /**
+   * If emitFiles is true, this option can be used to rename the emitted files.
+   * It accepts the following string replacements:
+   * - `[hash]` - The hash value of the file's contents
+   * - `[name]` - The name of the imported file (without its file extension)
+   * - `[extname]` - The extension of the imported file (including the leading .)
+   * - `[dirname]` - The parent directory name of the imported file (including trailing /)
+   *
+   * @default '[hash][extname]'
+   */
+  fileName?: string;
+  /**
+   * When using the [dirname] replacement in fileName, use this directory as the source directory
+   * from which to create the file path rather than the parent directory of the imported file.
+   *
+   * @default ''
+   *
+   * @example
+   * // src/path/to/file.js
+   * import png from './image.png';
+   *
+   * // config
+   * const config = {
+   *   fileName: '[dirname][hash][extname]',
+   *   sourceDir: path.join(__dirname, 'src')
+   * };
+   *
+   * // Emitted File:
+   * 'path/to/image.png'
+   */
+  sourceDir?: string;
+  /**
+   * The destination dir to copy assets, usually used to rebase the assets according to HTML files.
+   *
+   * @default ''
+   */
+  destDir?: string;
+}
+
+export function DefaultRollupUrlConfig(): RollupUrlConfig {
+  return {};
+}
+
+export interface BuiltinRollupPluginsConfig {
+  /**
+   * Enable Rollup Babel plugin.
+   *
+   * @default true
+   */
+  babel?: RollupBabelConfig | boolean;
+  /**
+   * Enable Rollup CommonJS plugin.
+   *
+   * @default true
+   */
+  cjs?: RollupCommonJSConfig | boolean;
+  /**
+   * Enable Rollup JSON plugin.
+   *
+   * @default true
+   */
+  json?: RollupJsonConfig | boolean;
+  /**
+   * Enable Rollup node resolve plugin.
+   *
+   * @default true
+   */
+  nodeResolve?: RollupNodeResolveConfig | boolean;
+  /**
+   * Enable Rollup Terser plugin.
+   *
+   * @default !env.DEBUG
+   */
+  terser?: RollupTerserConfig | boolean;
+  /**
+   * Enable Rollup URL plugin.
+   *
+   * @default true
+   */
+  url?: RollupUrlConfig | boolean;
+}
+
+export type ResolvedBuiltinRollupPluginsConfig = {
+  [Key in keyof BuiltinRollupPluginsConfig]: Exclude<BuiltinRollupPluginsConfig[Key], boolean>;
+};
+
+export function DefaultBuiltinRollupPluginsConfig(): ResolvedBuiltinRollupPluginsConfig {
+  return {
+    babel: DefaultRollupBabelConfig(),
+    cjs: DefaultRollupCommonJSConfig(),
+    json: DefaultRollupJsonConfig(),
+    nodeResolve: DefaultRollupNodeResolveConfig(),
+    terser: env.DEBUG ? undefined : DefaultRollupTerserConfig(),
+    url: DefaultRollupUrlConfig(),
+  };
+}
+
+export const BuiltinRollupPluginsMap: {
+  readonly [Key in keyof ResolvedBuiltinRollupPluginsConfig]-?: (
+    options: ResolvedBuiltinRollupPluginsConfig[Key],
+  ) => RollupPlugin;
+} = { babel, cjs, json, nodeResolve, terser, url };
+
+export interface RollupConfig extends RollupOptions, BuiltinRollupPluginsConfig {}
---
+++ packages/wc-bundler/src/config/rollup/load.ts
@@ -0,0 +1,79 @@
+import type { RollupOptions, Plugin as RollupPlugin } from 'rollup';
+import { asArray, iterargs } from '../../utils';
+import {
+  DefaultRollupBabelConfig,
+  ResolvedBuiltinRollupPluginsConfig,
+  RollupConfig,
+} from './interface';
+import {
+  BuiltinRollupPluginsMap,
+  DefaultBuiltinRollupPluginsConfig,
+  DefaultRollupCommonJSConfig,
+  DefaultRollupJsonConfig,
+  DefaultRollupNodeResolveConfig,
+  DefaultRollupTerserConfig,
+  DefaultRollupUrlConfig,
+} from './interface';
+
+const isKey = Object.prototype.hasOwnProperty as t.Object.prototype.hasOwnProperty;
+
+const BuiltinRollupPluginConfigGetters: Required<ResolvedBuiltinRollupPluginsConfig> = {
+  get babel() {
+    return DefaultRollupBabelConfig();
+  },
+  get cjs() {
+    return DefaultRollupCommonJSConfig();
+  },
+  get json() {
+    return DefaultRollupJsonConfig();
+  },
+  get nodeResolve() {
+    return DefaultRollupNodeResolveConfig();
+  },
+  get terser() {
+    return DefaultRollupTerserConfig();
+  },
+  get url() {
+    return DefaultRollupUrlConfig();
+  },
+};
+
+export function loadRollupConfig(configs: t.Readonly<RollupConfig>[]): RollupOptions;
+export function loadRollupConfig(...configs: t.Readonly<RollupConfig>[]): RollupOptions;
+export function loadRollupConfig(): RollupOptions {
+  const merged: RollupOptions = {};
+  const plugins: RollupPlugin[] = [];
+  const builtinplugins = DefaultBuiltinRollupPluginsConfig();
+
+  for (const config of iterargs<t.Readonly<RollupConfig>>(arguments)) {
+    for (const key in config) {
+      if (!isKey.call(config, key)) continue;
+
+      if (isKey.call(BuiltinRollupPluginConfigGetters, key)) {
+        if (config[key] === undefined) {
+          // ignore void preset config
+        } else if (!config[key]) {
+          builtinplugins[key] = undefined;
+        } else {
+          if (builtinplugins[key] === undefined)
+            Object.assign(builtinplugins, { [key]: BuiltinRollupPluginConfigGetters[key] });
+          if (typeof config[key] === 'object') Object.assign(builtinplugins[key], config[key]);
+        }
+      } else if (key === 'plugins') {
+        plugins.push.apply(plugins, asArray(config[key] || []) as RollupPlugin[]);
+      } else {
+        (merged as t.UnknownRecord)[key] = config[key];
+      }
+    }
+  }
+
+  for (const key in builtinplugins) {
+    if (isKey.call(builtinplugins, key) && builtinplugins[key] !== undefined) {
+      plugins.unshift(BuiltinRollupPluginsMap[key](builtinplugins[key] as never));
+    }
+  }
+
+  merged.plugins = plugins;
+
+  return merged;
+}
