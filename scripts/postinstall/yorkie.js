9c29a7a274d285d1222cfb3573ad1493b9284a49
imhele <work@imhele.com>
2021-08-20 22:21:52 +0800

    chore: remove is-ci

--- scripts/postinstall/yorkie.js
+++ scripts/postinstall/yorkie.js
@@ -1,19 +1,19 @@
-const isCI = require('is-ci');
-const installFrom = require('yorkie/src/install.js');
+const installFrom = require('yorkie/src/install');
+const isCI = require(require.resolve('is-ci', {
+  paths: [require.resolve('yorkie/src/install')],
+}));
 
 module.exports.yorkie = function yorkie() {
   console.log('\nRegister git hooks for yorkie...');
 
   if (isCI && !process.env.HUSKY_IGNORE_CI && !process.env.YORKIE_IGNORE_CI) {
-    console.log('CI detected, skipping Git hooks installation');
-    process.exit(0);
+    return console.log('CI detected, skipping Git hooks installation');
   }
 
   if (process.env.HUSKY_SKIP_INSTALL || process.env.YORKIE_SKIP_INSTALL) {
-    console.log(
+    return console.log(
       `env variable HUSKY_SKIP_INSTALL is set to ${process.env.HUSKY_SKIP_INSTALL}, skipping Git hooks installation`,
     );
-    process.exit(0);
   }
 
   console.log('setting up Git hooks');
